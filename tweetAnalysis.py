adb17eb00543761d7bb79a5c775ceed643c55529
Santhosh-S14 <santhoshaiyar97@gmail.com>
2021-02-04 15:58:02 +0530

    Preprocessing of tweets

--- tweetAnalysis.py
+++ tweetAnalysis.py
@@ -11,6 +11,7 @@ from wordcloud import WordCloud
 
 pd.set_option("display.max_colwidth", 200)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 train = pd.read_csv('train_E6oV3lV.csv')
 test = pd.read_csv('test_tweets_anuFYb8.csv')
@@ -42,4 +43,41 @@ plt.figure(figsize=(10, 7))
 plt.imshow(wordCloud, interpolation="bilinear")
 plt.axis('off')
 plt.show()
+
+rs_words = ' '.join([text for text in combine['tidy_tweet'][combine['label'] == 1]])
+wordCloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(rs_words)
+plt.figure(figsize=(10, 7))
+plt.imshow(wordCloud, interpolation="bilinear")
+plt.axis('off')
+plt.show()
+
+nrs_words = ' '.join([text for text in combine['tidy_tweet'][combine['label'] == 0]])
+wordCloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(nrs_words)
+plt.figure(figsize=(10, 7))
+plt.imshow(wordCloud, interpolation="bilinear")
+plt.axis('off')
+plt.show()
+
+
+def get_hashtags(tweet):
+    hashtags = []
+    for j in tweet:
+        hashtags.append(re.findall(r"#(\w+)", j))
+    return hashtags
+
+
+regular_words = get_hashtags(combine['tidy_tweet'][combine['label'] == 0])
+neg_words = get_hashtags(combine['tidy_tweet'][combine['label'] == 1])
+regular_words = sum(regular_words, [])
+neg_words = sum(neg_words, [])
+a = nltk.FreqDist(regular_words)
+d = pd.DataFrame({'Hashtag': list(a.keys()), 'Count': list(a.values())})
+d = d.nlargest(columns="Count", n=20)
+plt.figure(figsize=(16, 5))
+ax = sns.barplot(data=d, x="Hashtag", y="Count")
+b = nltk.FreqDist(neg_words)
+e = pd.DataFrame({'Hashtag': list(b.keys()), 'Count': list(b.values())})
+e = e.nlargest(columns="Count", n=20)
+plt.figure(figsize=(16, 5))
+bx = sns.barplot(data=e, x="Hashtag", y="Count")
 
